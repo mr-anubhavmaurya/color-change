de03b9412706c5f2592e28476840ad52ff3542f7
Anubhav <98anubhav.infotech@gmail.com>
2025-03-31 23:52:53 +0530

    background color changer game completed

---
+++ colorChanger/scripts.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        body.style.backgroundColor = button.id;
+    });
+});
+
